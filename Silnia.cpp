1849ad36e4015cfc5983e49156db432c3432338b
Emka5885 <masiak.emilia@o2.pl>
2023-02-20 20:45:26 +0100

    Silnia

---
+++ Silnia.cpp
@@ -0,0 +1,23 @@
+/*
+#include <iostream>
+
+long long factorial(int num)
+{
+    long long result = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int main()
+{
+    int number;
+    std::cout << "Number: "; std::cin >> number;
+
+    std::cout << "!" << number << " = " << factorial(number) << "\n";
+
+    return 0;
+}
+*/
